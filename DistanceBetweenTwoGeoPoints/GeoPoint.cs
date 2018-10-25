49d0cf9b8dc02256d90456eb4d303b88bcd01dd4
DaemonNikolay <nikolay-eckert@mail.ru>
2018-10-26 02:01:57 +0700

    Append drafts methods calculation distance to point static and not static; started using TDD

---
+++ DistanceBetweenTwoGeoPoints/GeoPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DistanceBetweenTwoGeoPoints
+{
+    public class GeoPoint
+    {
+        public GeoPoint(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        private double Latitude { get; set; }
+        private double Longitude { get; set; }
+
+        public double CalculationDistanceToPoint(GeoPoint second)
+        {
+            return double.MinValue;
+        }
+
+        public static double CalculationDistanceToPoint(double firstStart, double firstEnd, double secondStart, double secondEnd)
+        {
+            return double.MinValue;
+        }
+    }
+}
---
+++ Tests/GeoPointTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DistanceBetweenTwoGeoPoints;
+
+namespace Tests
+{
+    [TestClass]
+    public class GeoPointTests
+    {
+        GeoPoint start = new GeoPoint(4d, 5d);
+        GeoPoint end = new GeoPoint(2d, 6d);
+
+        [TestMethod]
+        public void CalculationDistanceToPointStatic()
+        {
+            var actual = GeoPoint.CalculationDistanceToPoint(1d, 2d, 3d, 4d);
+
+            Assert.AreEqual(expected: 5d, actual: actual);
+        }
+
+        [TestMethod]
+        public void CalculationDistanceToPoint()
+        {
+            var actual = start.CalculationDistanceToPoint(end);
+
+            Assert.AreEqual(expected: 5d, actual: actual);
+        }
+    }
+}
