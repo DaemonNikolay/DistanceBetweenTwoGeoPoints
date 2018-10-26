9f81151ca3fc8b2dab44e95c977ed05f69fa6020
DaemonNikolay <nikolay-eckert@mail.ru>
2018-10-27 00:01:57 +0700

    Create check points; create convert deg to radian

--- DistanceBetweenTwoGeoPoints/GeoPoint.cs
+++ DistanceBetweenTwoGeoPoints/GeoPoint.cs
@@ -19,12 +19,34 @@ namespace DistanceBetweenTwoGeoPoints
 
         public double CalculationDistanceToPoint(GeoPoint second)
         {
+            if (IsNotCorrectGeoPoint(second) && IsNotCorrectLatitude(Latitude) && IsNotCorrectLongitude(Longitude))
+            {
+                return double.MinValue;
+            }
+
+            var firstLatitude = ToRadian(Latitude);
+            var firstLongitude = ToRadian(Longitude);
+            var secondLatitude = ToRadian(second.Latitude);
+            var secondLongitude = ToRadian(second.Longitude);
+
             return double.MinValue;
         }
+
+
 
         public static double CalculationDistanceToPoint(double firstStart, double firstEnd, double secondStart, double secondEnd)
         {
             return double.MinValue;
+        }
+
+        private static double ToRadian(double deg) => deg == 0d ? 0d : Math.PI / 180 * deg;
+        private static bool IsNotCorrectLongitude(double longitude) => Math.Abs(longitude) <= 180d;
+
+        private static bool IsNotCorrectLatitude(double latitude) => Math.Abs(latitude) <= 90d;
+
+        private static bool IsNotCorrectGeoPoint(GeoPoint geoPoint)
+        {
+            return Math.Abs(geoPoint.Latitude) <= 90d && Math.Abs(geoPoint.Longitude) <= 180d;
         }
     }
 }
