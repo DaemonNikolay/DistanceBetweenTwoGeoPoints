60f1a9f7d8f77c9cc52044626166ae78f2737a1b
DaemonNikolay <nikolay-eckert@mail.ru>
2018-10-27 02:24:36 +0700

    Cleaning Program.cs

--- DistanceBetweenTwoGeoPoints/Program.cs
+++ DistanceBetweenTwoGeoPoints/Program.cs
@@ -1,19 +1,10 @@
-﻿using System;
-
-namespace DistanceBetweenTwoGeoPoints
+﻿namespace DistanceBetweenTwoGeoPoints
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            //var start = new GeoPoint(55.720224, 37.629893);
-            //var end = new GeoPoint(33.857982, 9.526866);
 
-            //var actual = start.CalculationDistanceToPoint(end);
-
-
-
-            //Console.WriteLine(actual);
         }
     }
 }
